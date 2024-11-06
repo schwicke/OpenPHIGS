d05727ec79ef2d17e146826c8bb55c0500fee57a
Ulrich Schwickerath <ulrich.schwickerath@gmail.com>
2024-11-06 09:00:25 +0100

    Fix tab character warnings in Fortran test

--- src/test_f/test_f1.f
+++ src/test_f/test_f1.f
@@ -18,63 +18,63 @@ C * You should have received a copy of the GNU Lesser General Public License
 C * along with Open PHIGS. If not, see <http://www.gnu.org/licenses/>.
 C *****************************************************************************
 
-	PROGRAM DRAWLINE
+      PROGRAM DRAWLINE
 
-C	Include PHIGS enumeration file
-	INCLUDE 'phigsf77.h'
+C     Include PHIGS enumeration file
+      INCLUDE 'phigsf77.h'
 
-C	Delcare arrays
-	REAL COLR(1:3)
-	REAL PXA(1:2)
-	REAL PYA(1:2)
+C     Delcare arrays
+      REAL COLR(1:3)
+      REAL PXA(1:2)
+      REAL PYA(1:2)
 
-C	Open PHIGS and a workstation
-	CALL POPPH(0, 0)
-	CALL POPWK(0, 0, 0)
+C      Open PHIGS and a workstation
+      CALL POPPH(0, 0)
+      CALL POPWK(0, 0, 0)
 
-C	Define line colour in workstation table
-	COLR(1) = 1.0
-	COLR(2) = 1.0
-	COLR(3) = 0.0
-	CALL PSCR(0, 1, 3, COLR)
+C      Define line colour in workstation table
+      COLR(1) = 1.0
+      COLR(2) = 1.0
+      COLR(3) = 0.0
+      CALL PSCR(0, 1, 3, COLR)
 
-C	Open structure
-	CALL POPST(0)
+C      Open structure
+      CALL POPST(0)
 
-C	Set text attributes
-	CALL PSCHH(0.055)
+C      Set text attributes
+      CALL PSCHH(0.055)
 
-C	Draw text
-	CALL PTX(0.22, 0.48, 'Hello Fortran!')
+C      Draw text
+      CALL PTX(0.22, 0.48, 'Hello Fortran!')
 
-C	Set line attributes
-	CALL PSPLCI(1)
-	CALL PSLN(PLDASD)
-	CALL PSLWSC(2.0)
+C      Set line attributes
+      CALL PSPLCI(1)
+      CALL PSLN(PLDASD)
+      CALL PSLWSC(2.0)
 
-C	Draw line from bottom-left to top-right
-	PXA(1) = 0.0
-	PYA(1) = 0.0
-	PXA(2) = 1.0
-	PYA(2) = 1.0
-	CALL PPL(2, PXA, PYA)
+C      Draw line from bottom-left to top-right
+      PXA(1) = 0.0
+      PYA(1) = 0.0
+      PXA(2) = 1.0
+      PYA(2) = 1.0
+      CALL PPL(2, PXA, PYA)
 
-C	Draw line from bottom-right to top-left
-	PXA(1) = 1.0
-	PYA(1) = 0.0
-	PXA(2) = 0.0
-	PYA(2) = 1.0
-	CALL PPL(2, PXA, PYA)
+C      Draw line from bottom-right to top-left
+      PXA(1) = 1.0
+      PYA(1) = 0.0
+      PXA(2) = 0.0
+      PYA(2) = 1.0
+      CALL PPL(2, PXA, PYA)
 
-C	Close structure
-	CALL PCLST
+C      Close structure
+      CALL PCLST
 
-C	Post structure to workstation
-	CALL PPOST(0, 0, 0.0)
+C      Post structure to workstation
+      CALL PPOST(0, 0, 0.0)
 
-C	Buisy loop
-	DO WHILE (1 .GT. 0)
-	END DO
+C      Buisy loop
+      DO WHILE (1 .GT. 0)
+      END DO
 
-	STOP
-	END
+      STOP
+      END
