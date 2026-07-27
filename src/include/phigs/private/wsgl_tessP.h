67308a653d219189a13672f57f4371f3ef299471
Param <param15.veer.singh@gmail.com>
2026-07-27 17:48:19 +0530

    chore: add missing license headers to tessellation files

--- src/include/phigs/private/wsgl_tessP.h
+++ src/include/phigs/private/wsgl_tessP.h
@@ -1,3 +1,23 @@
+/******************************************************************************
+*   DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+*
+*   This file is part of Open PHIGS
+*   Copyright (C) 2026 Paramveer Singh
+*
+*   Open PHIGS is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU Lesser General Public License as published by
+*   the Free Software Foundation, either version 2.1 of the License, or
+*   (at your option) any later version.
+*
+*   Open PHIGS is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU Lesser General Public License for more details.
+*
+*   You should have received a copy of the GNU Lesser General Public License
+*   along with Open PHIGS. If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
 #ifndef _wsgl_tessP_h
 #define _wsgl_tessP_h
 
