763dff208624036f0470792b2234deda868e295f
Ulrich Schwickerath <ulrich.schwickerath@cern.ch>
2026-08-18 15:01:27 +0200

    Fix: Unify formatting in wsgl_tess.c

--- src/libphigs/wsgl/wsgl_tess.c
+++ src/libphigs/wsgl/wsgl_tess.c
@@ -1,24 +1,24 @@
 /******************************************************************************
-*   DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
-*
-*   This file is part of Open PHIGS
-*   Copyright (C) 2026 Paramveer Singh
-*
-*   Open PHIGS is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU Lesser General Public License as published by
-*   the Free Software Foundation, either version 2.1 of the License, or
-*   (at your option) any later version.
-*
-*   Open PHIGS is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU Lesser General Public License for more details.
-*
-*   You should have received a copy of the GNU Lesser General Public License
-*   along with Open PHIGS. If not, see <http://www.gnu.org/licenses/>.
-*
-*   Changes: Copyright (C) C 2026 CERN
-******************************************************************************/
+ *   DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ *
+ *   This file is part of Open PHIGS
+ *   Copyright (C) 2026 Paramveer Singh
+ *
+ *   Open PHIGS is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 2.1 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Open PHIGS is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with Open PHIGS. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *   Changes: Copyright (C) C 2026 CERN
+ ******************************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -41,181 +41,181 @@
 #endif
 /**
    \brief Helper function to keep track of resource allocations in tesselation shaders
- */
+*/
 static int pool_add(Wsgl_tess_pool *pool, Wsgl_tess_vertex *v)
 {
-   if (pool->count == pool->capacity) {
-      int newcap = (pool->capacity == 0) ? 16 : pool->capacity * 2;
-      Wsgl_tess_vertex **tmp = (Wsgl_tess_vertex **)
-         realloc(pool->verts, newcap * sizeof(Wsgl_tess_vertex *));
-      if (tmp == NULL)
-         return 0;                 /* old pool->verts still valid */
-      pool->verts = tmp;
-      pool->capacity = newcap;
-   }
-   pool->verts[pool->count++] = v;
-   return 1;
+  if (pool->count == pool->capacity) {
+    int newcap = (pool->capacity == 0) ? 16 : pool->capacity * 2;
+    Wsgl_tess_vertex **tmp = (Wsgl_tess_vertex **)
+      realloc(pool->verts, newcap * sizeof(Wsgl_tess_vertex *));
+    if (tmp == NULL)
+      return 0;                 /* old pool->verts still valid */
+    pool->verts = tmp;
+    pool->capacity = newcap;
+  }
+  pool->verts[pool->count++] = v;
+  return 1;
 }
 
 /**
    \brief Helper function to cleanup the resource pool used in tesselation shaders
- */
+*/
 static void pool_free(Wsgl_tess_pool *pool)
 {
-   int i;
-   for (i = 0; i < pool->count; i++)
-      free(pool->verts[i]);
-   free(pool->verts);
-   pool->verts = NULL;
-   pool->count = 0;
-   pool->capacity = 0;
+  int i;
+  for (i = 0; i < pool->count; i++)
+    free(pool->verts[i]);
+  free(pool->verts);
+  pool->verts = NULL;
+  pool->count = 0;
+  pool->capacity = 0;
 }
 
 static void CALLBACK tessBeginCB(GLenum which) {
-    glBegin(which);
+  glBegin(which);
 }
 
 static void CALLBACK tessEndCB() {
-    glEnd();
+  glEnd();
 }
 
 static void CALLBACK tessEdgeFlagCB(GLboolean flag) {
-    glEdgeFlag(flag);
+  glEdgeFlag(flag);
 }
 
 static void CALLBACK tessVertexCB(void *data) {
-    Wsgl_tess_vertex *v = (Wsgl_tess_vertex *)data;
-    if (v->has_norm) {
-        glNormal3fv(v->norm);
-        wsgl_set_current_normal((float)v->norm[0], (float)v->norm[1], (float)v->norm[2]);
-    }
-    if (v->apply_cb) {
-        v->apply_cb(v);
-    }
-    glVertex3dv(v->pos);
+  Wsgl_tess_vertex *v = (Wsgl_tess_vertex *)data;
+  if (v->has_norm) {
+    glNormal3fv(v->norm);
+    wsgl_set_current_normal((float)v->norm[0], (float)v->norm[1], (float)v->norm[2]);
+  }
+  if (v->apply_cb) {
+    v->apply_cb(v);
+  }
+  glVertex3dv(v->pos);
 }
 
-static void CALLBACK tessCombineCB(GLdouble coords[3], 
-                                   void *vertex_data[4], 
-                                   GLfloat weight[4], 
+static void CALLBACK tessCombineCB(GLdouble coords[3],
+                                   void *vertex_data[4],
+                                   GLfloat weight[4],
                                    void **outData,
                                    void *polygon_data) {
-    /* Basic combine callback to prevent crashing on self-intersecting polygons */
-    Wsgl_tess_pool *pool = (Wsgl_tess_pool *) polygon_data;
-    Wsgl_tess_vertex *new_vert = (Wsgl_tess_vertex *)malloc(sizeof(Wsgl_tess_vertex));
-    if (new_vert) {
-        memset(new_vert, 0, sizeof(Wsgl_tess_vertex));
-        new_vert->pos[0] = coords[0];
-        new_vert->pos[1] = coords[1];
-        new_vert->pos[2] = coords[2];
-        if (vertex_data[0]) {
-            Wsgl_tess_vertex *v0 = (Wsgl_tess_vertex *)vertex_data[0];
-            new_vert->apply_cb = v0->apply_cb;
-            new_vert->ws = v0->ws;
-            new_vert->colr_type = v0->colr_type;
-            new_vert->colr = v0->colr;
-            new_vert->ast = v0->ast;
-            new_vert->has_norm = v0->has_norm;
-            if (new_vert->has_norm) {
-                new_vert->norm[0] = v0->norm[0];
-                new_vert->norm[1] = v0->norm[1];
-                new_vert->norm[2] = v0->norm[2];
-            }
-        }
-        if (!pool_add(pool, new_vert)) {
-            free(new_vert);           /* can't track it — don't leak it */
-            *outData = vertex_data[0];
-            return;
-        }
-        *outData = new_vert;
-    } else {
-        *outData = vertex_data[0];
+  /* Basic combine callback to prevent crashing on self-intersecting polygons */
+  Wsgl_tess_pool *pool = (Wsgl_tess_pool *) polygon_data;
+  Wsgl_tess_vertex *new_vert = (Wsgl_tess_vertex *)malloc(sizeof(Wsgl_tess_vertex));
+  if (new_vert) {
+    memset(new_vert, 0, sizeof(Wsgl_tess_vertex));
+    new_vert->pos[0] = coords[0];
+    new_vert->pos[1] = coords[1];
+    new_vert->pos[2] = coords[2];
+    if (vertex_data[0]) {
+      Wsgl_tess_vertex *v0 = (Wsgl_tess_vertex *)vertex_data[0];
+      new_vert->apply_cb = v0->apply_cb;
+      new_vert->ws = v0->ws;
+      new_vert->colr_type = v0->colr_type;
+      new_vert->colr = v0->colr;
+      new_vert->ast = v0->ast;
+      new_vert->has_norm = v0->has_norm;
+      if (new_vert->has_norm) {
+        new_vert->norm[0] = v0->norm[0];
+        new_vert->norm[1] = v0->norm[1];
+        new_vert->norm[2] = v0->norm[2];
+      }
     }
+    if (!pool_add(pool, new_vert)) {
+      free(new_vert);           /* can't track it — don't leak it */
+      *outData = vertex_data[0];
+      return;
+    }
+    *outData = new_vert;
+  } else {
+    *outData = vertex_data[0];
+  }
 }
 
 static void CALLBACK tessErrorCB(GLenum errorCode) {
-    const GLubyte *errorStr;
-    errorStr = gluErrorString(errorCode);
-    /* fprintf(stderr, "OpenPHIGS GLU Tessellation Error: %s\n", errorStr); */
+  const GLubyte *errorStr;
+  errorStr = gluErrorString(errorCode);
+  /* fprintf(stderr, "OpenPHIGS GLU Tessellation Error: %s\n", errorStr); */
 }
 
 void wsgl_draw_tess_polygon(Wsgl_tess_vertex *vertices, int num_vertices, int record_geom_flag)
 {
-    GLUtesselator *tess;
-    int i;
-    int vertex_indices[MAX_VERTICES];
-    int normal_indices[MAX_VERTICES];
-    int n_vertices = 0;
-    int n_normals = 0;
-    GLboolean orig_depth_mask;
-    GLfloat cur_color[4];
-    int has_transparency = 0;
+  GLUtesselator *tess;
+  int i;
+  int vertex_indices[MAX_VERTICES];
+  int normal_indices[MAX_VERTICES];
+  int n_vertices = 0;
+  int n_normals = 0;
+  GLboolean orig_depth_mask;
+  GLfloat cur_color[4];
+  int has_transparency = 0;
 
-    tess = gluNewTess();
-    if (!tess) return;
-    if (record_geom_flag && num_vertices > MAX_VERTICES) {
-        record_geom_flag = 0;      /* or clamp, or allocate */
+  tess = gluNewTess();
+  if (!tess) return;
+  if (record_geom_flag && num_vertices > MAX_VERTICES) {
+    record_geom_flag = 0;      /* or clamp, or allocate */
+  }
+
+  Wsgl_tess_pool pool = { NULL, 0, 0 };
+
+  gluTessCallback(tess, GLU_TESS_BEGIN, (void (CALLBACK *)())tessBeginCB);
+  gluTessCallback(tess, GLU_TESS_END, (void (CALLBACK *)())tessEndCB);
+  gluTessCallback(tess, GLU_TESS_VERTEX, (void (CALLBACK *)())tessVertexCB);
+  gluTessCallback(tess, GLU_TESS_ERROR, (void (CALLBACK *)())tessErrorCB);
+  gluTessCallback(tess, GLU_TESS_COMBINE, (void (CALLBACK *)())tessCombineCB);
+  gluTessCallback(tess, GLU_TESS_EDGE_FLAG, (void (CALLBACK *)())tessEdgeFlagCB);
+  gluTessCallback(tess, GLU_TESS_COMBINE_DATA, (void (CALLBACK *)()) tessCombineCB);
+
+  /* Determine if depth writing should be disabled for order-independent transparency */
+  glGetBooleanv(GL_DEPTH_WRITEMASK, &orig_depth_mask);
+
+  if (num_vertices > 0 && vertices[0].apply_cb) {
+    if (vertices[0].colr_type == PMODEL_RGBA && vertices[0].colr.direct.rgba.alpha < 1.0f) {
+      has_transparency = 1;
     }
-
-    Wsgl_tess_pool pool = { NULL, 0, 0 };
- 
-    gluTessCallback(tess, GLU_TESS_BEGIN, (void (CALLBACK *)())tessBeginCB);
-    gluTessCallback(tess, GLU_TESS_END, (void (CALLBACK *)())tessEndCB);
-    gluTessCallback(tess, GLU_TESS_VERTEX, (void (CALLBACK *)())tessVertexCB);
-    gluTessCallback(tess, GLU_TESS_ERROR, (void (CALLBACK *)())tessErrorCB);
-    gluTessCallback(tess, GLU_TESS_COMBINE, (void (CALLBACK *)())tessCombineCB);
-    gluTessCallback(tess, GLU_TESS_EDGE_FLAG, (void (CALLBACK *)())tessEdgeFlagCB);
-    gluTessCallback(tess, GLU_TESS_COMBINE_DATA, (void (CALLBACK *)()) tessCombineCB);
-
-    /* Determine if depth writing should be disabled for order-independent transparency */
-    glGetBooleanv(GL_DEPTH_WRITEMASK, &orig_depth_mask);
-    
-    if (num_vertices > 0 && vertices[0].apply_cb) {
-        if (vertices[0].colr_type == PMODEL_RGBA && vertices[0].colr.direct.rgba.alpha < 1.0f) {
-            has_transparency = 1;
-        }
-    } else {
-        glGetFloatv(GL_CURRENT_COLOR, cur_color);
-        if (cur_color[3] < 1.0f) {
-            has_transparency = 1;
-        }
+  } else {
+    glGetFloatv(GL_CURRENT_COLOR, cur_color);
+    if (cur_color[3] < 1.0f) {
+      has_transparency = 1;
     }
-    
-    if (has_transparency && orig_depth_mask == GL_TRUE) {
-        glDepthMask(GL_FALSE);
+  }
+
+  if (has_transparency && orig_depth_mask == GL_TRUE) {
+    glDepthMask(GL_FALSE);
+  }
+
+  gluTessBeginPolygon(tess, &pool);
+  gluTessBeginContour(tess);
+
+  for (i = 0; i < num_vertices; i++) {
+    gluTessVertex(tess, vertices[i].pos, (void *)&vertices[i]);
+
+    if (record_geom_flag) {
+      vertex_indices[n_vertices] = wsgl_add_vertex((float)vertices[i].pos[0],
+                                                   (float)vertices[i].pos[1],
+                                                   (float)vertices[i].pos[2]);
+      n_vertices++;
+      normal_indices[n_normals] = wsgl_add_normal(current_normal.x,
+                                                  current_normal.y,
+                                                  current_normal.z);
+      n_normals++;
     }
+  }
 
-    gluTessBeginPolygon(tess, &pool);
-    gluTessBeginContour(tess);
+  gluTessEndContour(tess);
+  gluTessEndPolygon(tess);
+  pool_free(&pool);
+  gluDeleteTess(tess);
 
-    for (i = 0; i < num_vertices; i++) {
-        gluTessVertex(tess, vertices[i].pos, (void *)&vertices[i]);
+  /* Restore default edge flag state for subsequent rendering */
+  glEdgeFlag(GL_TRUE);
 
-        if (record_geom_flag) {
-            vertex_indices[n_vertices] = wsgl_add_vertex((float)vertices[i].pos[0],
-                                                         (float)vertices[i].pos[1],
-                                                         (float)vertices[i].pos[2]);
-            n_vertices++;
-            normal_indices[n_normals] = wsgl_add_normal(current_normal.x,
-                                                        current_normal.y,
-                                                        current_normal.z);
-            n_normals++;
-        }
-    }
+  if (has_transparency && orig_depth_mask == GL_TRUE) {
+    glDepthMask(GL_TRUE);
+  }
 
-    gluTessEndContour(tess);
-    gluTessEndPolygon(tess);
-    pool_free(&pool);
-    gluDeleteTess(tess);
-
-    /* Restore default edge flag state for subsequent rendering */
-    glEdgeFlag(GL_TRUE);
-
-    if (has_transparency && orig_depth_mask == GL_TRUE) {
-        glDepthMask(GL_TRUE);
-    }
-
-    if (record_geom_flag && n_vertices > 0) {
-        wsgl_add_geometry(GEOM_FACE, vertex_indices, normal_indices, n_vertices);
-    }
+  if (record_geom_flag && n_vertices > 0) {
+    wsgl_add_geometry(GEOM_FACE, vertex_indices, normal_indices, n_vertices);
+  }
 }
